6e3b83356740a89ffc6c7f6d6f3c2f237df4fc77
TayAutumn <168868076+TayAutumn@users.noreply.github.com>
2024-06-15 21:23:55 -0400

    created function for task card and render task list

--- js/script.js
+++ js/script.js
@@ -7,14 +7,48 @@ function generateTaskId() {
     return nextId++;
 }
 
+// Save tasks and nextId to localStorage
+function saveToLocalStorage() {
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    localStorage.setItem("nextId", JSON.stringify(nextId));
+}
+
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    let cardColor = '';
+    constdeadline = dayjs(task.deadline)
+    const now = dayjs();
 
+    if (now.isAfter(deadline)) {
+        cardColor = 'bg-danger text-white';
+    } else if (now.add(3, 'day').isAfter(deadline)) {
+        cardColor = 'bg-warning';
+    }
+    
 }
+
+return $(`
+    <div class="card mb-3 ${cardColor}" data-id="${task.id}">
+    <div class="card-body">
+        <h5 class="card-title">${task.title}</h5>
+        <p class="card-text">${task.description}</p>
+        <p class="card-text"><small>Deadline: ${task.deadline}</small></p>
+        <button class="btn btn-danger btn-sm delete-task">Delete</button>
+    </div>
+    </div>
+`);
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+    $('.task-cards').empty();
+        taskList.forEach(task => {
+    const taskCard = createTaskCard(task);
+    $(`#${task.status}-cards`).append(taskCard);
+});
+    $('.task-cards .card').draggable({
+        revert: 'invalid',
+        helper: 'clone'
+});
 }
 
 // Todo: create a function to handle adding a new task
